c34afcad3af7df5f1af859a5a4df92cb9c8bd3a2
jarielmatos <jariel.matos@KIPPNYCCP.ORG>
2017-05-16 13:25:49 -0400

    Create application.js

---
+++ application.js
@@ -0,0 +1,22 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  $(document).ready(function(){ 
+   $("p").click(function(){
+    $("h1").css("border","1px solid blue") 
+     $("h3").css("border","1px solid blue") 
+      
+     })
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
