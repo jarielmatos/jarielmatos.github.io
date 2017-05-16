d019e412a8adb2679439e5be47c4af09c4103fb8
jarielmatos <jariel.matos@KIPPNYCCP.ORG>
2017-05-16 13:35:38 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,11 +1,9 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  $(document).ready(function(){ 
+$(document).ready(function(){ 
    $("p").click(function(){
-    $("h1").css("border","1px solid blue") 
-     $("h3").css("border","1px solid blue") 
-      
-     })
-}) 
+    $("h1").css("border","1px solid blue");
+     $("h3").css("border","1px solid blue"); 
+     });
+});
 
 
 
