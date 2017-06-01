a817f7c5063ae844f0916d63441ebc0974605606
jarielmatos <jariel.matos@KIPPNYCCP.ORG>
2017-06-01 13:21:48 -0400

    Update application.js

--- game/application.js
+++ game/application.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
     $("#submit").click(function(){
       var name= $("input[name=ok]".val();
-        $("#response).html(name);
+        $("#response").html(name);
 }
 
 });
