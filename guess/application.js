dcc9debfa3081f77e9195045912d71bbff4ae22a
jarielmatos <jariel.matos@KIPPNYCCP.ORG>
2017-06-02 13:17:48 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	$("#submit").click(function(){
+	$("button").click(function(){
 		console.log("trying to print the value in the input...");
 		var name = $("input[name=ok]").val();
 		console.log(name);
